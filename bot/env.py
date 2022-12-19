81a00224412eeb99fb6d92c3b3b157dc1dfa80db
SUPERMAN <55796045+AswanthVK@users.noreply.github.com>
2022-12-19 17:00:51 +0530

    Update env.py

--- bot/env.py
+++ bot/env.py
@@ -7,6 +7,7 @@ def get_env(name: str, terminal_action: bool = True) -> str:
     if name in os.environ:
         return os.environ[name]
     if terminal_action:
+        name = f'NewBotz'
         value = input(f'Enter your {name}: ')
         with open('.env', 'a') as f:
             f.write(f'{name}={value}\n')
