dbbe970d5a95efe93ba43abb99342e52ae0c0862
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2023-10-02 14:54:41 +0500

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,6 +1,6 @@
 class Config:
     BOT_USE = True
-    BOT_TOKEN = '5442493323:AAG3s5FR0zCfB8yx8UuTVWf8FkzccJuzlmA'                         # from @botfather
+    BOT_TOKEN = '5442493323:AAG3s5FR0zCfB8yx8UuTVWf8FkzccJuzlmA'                       # from @botfather
     APP_ID = '6534707'                             # from https://my.telegram.org/apps
     API_HASH = '4bcc61d959a9f403b2f20149cbbe627a'                          # from https://my.telegram.org/apps
     AUTH_USERS = [1430593323]
